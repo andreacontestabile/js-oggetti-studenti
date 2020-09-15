5124742333832a0a6dd6965f51db7197dc0b048e
Andrea Contestabile <andreacontest@gmail.com>
2020-09-15 17:44:22 +0200

    (Handlebars) Interfaccia con template ed evento click

--- js/script.js
+++ js/script.js
@@ -1,66 +1,107 @@
-/* 1. Creare un oggetto che descriva uno studente con le seguenti proprietà:
-nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà. */
+$(document).ready(function() {
 
-// Creo un nuovo oggetto studente:
-var unoStudente = {
-  "nome": "Luca",
-  "cognome": "Messina",
-  "eta": 23
-};
+  // HANDLEBARS
 
-// Ciclo all'interno dell'oggetto, stampando ognuna delle sue proprietà
-for (var key in unoStudente) {
-  console.log(key + " : " + unoStudente[key]);
-}
+  var source = $("#student-template").html();
+  var template = Handlebars.compile(source);
+
+  var context = {
+    title: "Scheda Studente"
+  };
 
 
+  /* 1. Creare un oggetto che descriva uno studente con le seguenti proprietà:
+  nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà. */
 
-/* 2. Creare un array di oggetti di studenti.
-Ciclare su tutti gli studenti e stampare per ognuno nome e cognome */
-
-// Creo un array che contiene diversi oggetti studente
-var alcuniStudenti = [
-  {
-    "nome": "Sara",
-    "cognome": "Longo",
-    "eta": 21
-  },
-  {
-    "nome": "Andrea",
-    "cognome": "Rossi",
-    "eta": 22
-  }, {
-    "nome": "Franco",
-    "cognome": "Giurato",
-    "eta": 25
-  },
-  {
-    "nome": "Laura",
-    "cognome": "Ferrari",
+  // Creo un nuovo oggetto studente:
+  var unoStudente = {
+    "nome": "Luca",
+    "cognome": "Messina",
     "eta": 23
-  },
-  {
-    "nome": "Alessandro",
-    "cognome": "Testa",
-    "eta": 22
+  };
+
+  context.studente = unoStudente;
+
+  var html = template(context);
+  $('.app').append(html);
+
+  // Ciclo all'interno dell'oggetto, stampando ognuna delle sue proprietà
+  for (var key in unoStudente) {
+    console.log(key + " : " + unoStudente[key]);
   }
-];
-
-// Ciclo all'interno dell'array e per ognuno degli oggetti, stampo nome e cognome
-for (var i = 0; i < alcuniStudenti.length; i++) {
-  var studente = alcuniStudenti[i];
-  console.log(studente.nome + " " + studente.cognome);
-}
 
 
-/* 3. Dare la possibilità all’utente attraverso 3 prompt di aggiungere
-un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
- var nuovoStudente = {};
- nuovoStudente.nome = prompt("Inserire il nome del nuovo studente");
- nuovoStudente.cognome = prompt("Inserire il cognome del nuovo studente");
- nuovoStudente.eta = parseInt(prompt("Inserire l'età del nuovo studente"));
+  /* 2. Creare un array di oggetti di studenti.
+  Ciclare su tutti gli studenti e stampare per ognuno nome e cognome */
 
- alcuniStudenti.push(nuovoStudente);
+  // Creo un array che contiene diversi oggetti studente
+  var alcuniStudenti = [
+    {
+      "nome": "Sara",
+      "cognome": "Longo",
+      "eta": 21
+    },
+    {
+      "nome": "Andrea",
+      "cognome": "Rossi",
+      "eta": 22
+    }, {
+      "nome": "Franco",
+      "cognome": "Giurato",
+      "eta": 25
+    },
+    {
+      "nome": "Laura",
+      "cognome": "Ferrari",
+      "eta": 23
+    },
+    {
+      "nome": "Alessandro",
+      "cognome": "Testa",
+      "eta": 22
+    }
+  ];
 
- console.log(alcuniStudenti);
+  // Ciclo all'interno dell'array e per ognuno degli oggetti, stampo nome e cognome
+  for (var i = 0; i < alcuniStudenti.length; i++) {
+    var studente = alcuniStudenti[i];
+    console.log(studente.nome + " " + studente.cognome);
+  }
+
+  // Ciclo all'interno dell'array e per ognuno degli oggetti, aggiorno il context
+  for (var i = 0; i < alcuniStudenti.length; i++) {
+    var studente = alcuniStudenti[i];
+    context.studente = studente;
+    var html = template(context);
+    $('.app').append(html);
+  }
+
+  /* 3. Dare la possibilità all’utente attraverso 3 prompt di aggiungere
+  un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+   $("#new-student").click(function(){
+
+     var nuovoStudente = {};
+     nuovoStudente.nome = prompt("Inserire il nome del nuovo studente");
+     nuovoStudente.cognome = prompt("Inserire il cognome del nuovo studente");
+     nuovoStudente.eta = parseInt(prompt("Inserire l'età del nuovo studente"));
+
+     context.studente = nuovoStudente;
+
+     var html = template(context);
+     $('.app').append(html);
+
+   });
+
+
+   // var nuovoStudente = {};
+   // nuovoStudente.nome = prompt("Inserire il nome del nuovo studente");
+   // nuovoStudente.cognome = prompt("Inserire il cognome del nuovo studente");
+   // nuovoStudente.eta = parseInt(prompt("Inserire l'età del nuovo studente"));
+   //
+   // alcuniStudenti.push(nuovoStudente);
+   //
+   // console.log(alcuniStudenti);
+
+});
